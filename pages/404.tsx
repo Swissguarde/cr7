6fa607a24ad12549aa97274d6c80218c4e52343b
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-01-04 21:01:37 +0000

    added 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,22 @@
+import Container from "../components/Container";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Seo from "../components/Seo";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <div>
+        <Seo title="404 - CR7" />
+        <Header />
+        <Container className="mt-20">
+          <h2>Sorry! Page Not Found!</h2>
+        </Container>
+        <div className="fixed bottom-0 w-full">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 };
 export default NotFound;
